402754e230f59ac70c7af7c337ecf520f7c38a62
Adilson Tavares <adilsontavares@MacBook-Pro-de-Adilson.local>
2017-08-10 12:02:59 -0300

    Simples programa criando um pacote e mostrando na tela

--- src/ntpdate.php
+++ src/ntpdate.php
@@ -1,8 +1,32 @@
 <?php
 
+    require('packet.php');
+
     // Unsure there is a URL parameter
     $argc == 2 or die('USAGE: <URL>');
     $url = $argv[1];
+    $port = 123;
 
-    echo 'Hello, world!'
+    send($url, $port);
+
+    function send($url, $port) {
+
+        $version = 4;
+        $mode = 3;
+
+        $packet = new Packet(48);
+        $packet->write(2, 5, $version);
+        $packet->write(5, 8, $mode);
+        $packet->debug(4);
+
+        $data = $packet->data();
+        $datalen = $packet->datalen();
+
+        // echo $packet->representation() . "\n";
+
+        // $sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+        // $res = socket_sendto($sock, $data, $datalen, 0x100, $url, $port);
+
+        // return $res !== FALSE;
+    }
 ?>
