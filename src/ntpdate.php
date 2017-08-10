f7223f726c5f8899dd2a7db1067d31f417c71d7c
Adilson Tavares <adilsontavares@MacBook-Pro-de-Adilson.local>
2017-08-10 13:36:18 -0300

    Recuperando o horário do servidor

--- src/ntpdate.php
+++ src/ntpdate.php
@@ -10,7 +10,19 @@
     send($sock, $url, $port);
     receive($sock, $url, $port);
 
+    echo "T1 = $t1\n";
+    echo "T2 = $t2\n";
+    echo "T3 = $t3\n";
+    echo "T4 = $t4\n";
+
+    echo "\n";
+
+    echo "Local:    " . date("d/M/y H:i:s", $t4) . "\n";
+    echo "Servidor: " . date("d/M/y H:i:s", $t3) . "\n";
+
     function send(&$sock, $url, $port) {
+
+        global $t1;
 
         $version = 4;
         $mode = 3;
@@ -24,14 +36,25 @@
 
         $sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
         socket_connect($sock, $url, $port) or die('Could not connect to host.');
+
+        $t1 = time();
         socket_send($sock, $data, strlen($data), 0);
     }
 
     function receive(&$sock, $url, $port) {
 
+        global $t2, $t3, $t4;
+
         socket_recv($sock, $data, 48, MSG_WAITALL);
+        $t4 = time();
+
         socket_close($sock);
+        
         $packet = Packet::withData($data, 48);
         $packet->debug();
+
+        $res = unpack("N12", $packet->data());
+        $t2 = (double)$res[9] + 1.0 / (double)$res[10];
+        $t3 = (double)$res[11] + 1.0 / (double)$res[12];
     }
 ?>
